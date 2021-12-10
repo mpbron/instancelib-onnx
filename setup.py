ceb75584635c885e7d94622938eaf1a6e99ee12e
Michiel Bron <m.p.bron@uu.nl>
2021-12-10 10:46:21 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setuptools.setup( # type: ignore
     name="instancelib-onnx",
-    version="0.0.8",
+    version="0.0.9",
     description="A wrapper for ONNX models that adheres to instancelib",
     long_description=README,
     long_description_content_type="text/markdown",
